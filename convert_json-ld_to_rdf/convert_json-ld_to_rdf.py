57496f9a78c10e2c2b452ad86b111bde52372db9
unknown <pierre.centa@edu.devinci.fr>
2021-03-23 11:55:25 +0100

    handicaps gares nov 2020

--- convert_json-ld_to_rdf/convert_json-ld_to_rdf.py
+++ convert_json-ld_to_rdf/convert_json-ld_to_rdf.py
@@ -1,12 +1,14 @@
+import rdflib
+import json
 from rdflib import Graph
 
-file = open("./json-ld_toConvert.json", "r").read()
+file = open("./handicaps_tojson_ld.json", "r").read()
 
 graph = Graph().parse(data=file, format='json-ld')
 
 rdf = graph.serialize(format='turtle', indent=4)
 
-with open('./result.ttl', 'wb') as fp:
+with open('./result2.ttl', 'wb') as fp:
     fp.write(rdf)
 
 print('Done.')
