d48e875ae840312359879251d905f7da520f4969
hydralom <32839160+hydralom@users.noreply.github.com>
2021-03-24 00:04:14 +0100

    update

--- symfony/semantic_project/src/Controller/Carte.php
+++ symfony/semantic_project/src/Controller/Carte.php
@@ -15,24 +15,12 @@ class Carte extends AbstractController
 {
 
     /**
-     * @Route("/trainstations/{api}/{city}", name="trainstations", defaults={"city"="Z","api"="Z"}))
-     *
-     * @param string $city
+     * @Route("/carte", name="carte_all_in_one")
      *
      * @return Response
      */
-    public function TrainStationsCity(string $city): Response
+    public function AllInOne(): Response
     {
-        $queryTs = "
-           SELECT ?nom ?lat ?lon
-           WHERE {
-               ?subject <http://confinos.fr/train_stations#nom_gare_pretty> ?nom .
-               ?subject <http://confinos.fr/train_stations#latitude> ?lat .
-               ?subject <http://confinos.fr/train_stations#longitude> ?lon .
-           }
-           ORDER BY ASC(?nom)
-        ";
-
         return $this->render('carte/all_in_one.html.twig', [
         ]);
     }
